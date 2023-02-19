b2b6fff8f54a77a7ee74db638be4af3ce0bcbe2c
Andreas Pedersen <31003640+AndreasP-92@users.noreply.github.com>
2023-02-19 22:01:43 +0100

    Co-authored-by: Abdulahi Roble <abdulahiroble@users.noreply.github.com>

--- initialTask/python/routes/routes.py
+++ initialTask/python/routes/routes.py
@@ -16,9 +16,12 @@ def _():
     return {"message": "This is my second route"}
 
 
-@routes.post("/uploadfile/")
-async def create_upload_file(file: UploadFile = File(...)):
-    contents = await file.read()
-    # You can then use the contents variable to read the contents of the file
-    # and do something with it, such as parse it as a CSV using the csv module.
-    return {"filename": file.filename}
+@routes.get("/csv")
+async def read_csv(response: Response):
+    with open("files/me.csv", newline='') as csvfile:
+        csvreader = csv.reader(csvfile, delimiter=',', quotechar='"')
+        data = []
+        for row in csvreader:
+            data.append(row)
+    response.headers["Content-Disposition"] = "attachment; filename=data.csv"
+    return {"data": data}
